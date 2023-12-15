5ce5b80bb40009edb0a81400366807c9dce230b1
gorgeousPotato <lanauzbekov@gmail.com>
2023-12-14 23:26:44 -0800

    graph

---
+++ src/pages/MoodGraph/MoodGraph.js
@@ -0,0 +1,10 @@
+import {Bar} from 'react-chartjs-2';
+import "./MoodGraph.css"
+
+export default function MoodGraph({moods}) {
+  return (
+    <div className='MoodGraph'>
+      <h1>graph</h1>
+    </div>
+  );
+}
